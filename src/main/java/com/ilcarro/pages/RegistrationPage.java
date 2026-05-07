52fddec70a2ba5a820a4b3f264e700c0b208a7f5
JennyKlein <jenny.klein@mail.de>
2026-05-07 19:08:44 +0200

    add registration feature

---
+++ src/main/java/com/ilcarro/pages/RegistrationPage.java
@@ -0,0 +1,58 @@
+package com.ilcarro.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class RegistrationPage extends BasePage{
+    public RegistrationPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(css = "[name='firstName']")
+    WebElement inputName;
+    @FindBy(css = "[name='lastName']")
+    WebElement inputLastname;
+    @FindBy(css = "[type='email']")
+    WebElement inputEmail;
+    @FindBy(css = "[name='password']")
+    WebElement inputPassword;
+    public RegistrationPage enterRegistrationDetails(String name, String lastname, String email, String password) {
+        type(inputName, name);
+        type(inputLastname, lastname);
+        type(inputEmail,email);
+        type(inputPassword,password);
+        return this;
+    }
+    @FindBy(css = "[name='termsOfUse']")
+    WebElement checkBox;
+    public RegistrationPage checkTheBoxToAgree() {
+        click(checkBox);
+        return this;
+    }
+
+    @FindBy(css = "[type='submit']")
+    WebElement yallaButton;
+    public RegistrationPage clickOnYallaButton() {
+        click(yallaButton);
+        return this;
+    }
+
+    @FindBy(css = "h3")
+    WebElement messageText;
+    public RegistrationPage successMessage(String message) {
+        assert shouldHaveText(messageText,message,5);
+        return this;
+    }
+
+    public RegistrationPage userEntersExistingData() {
+        return enterRegistrationDetails("Olga", "Smith", "myliberty3@ukr.net", "Aa123456!");
+    }
+
+    @FindBy(css = "h3")
+    WebElement message;
+    public RegistrationPage errorMessage(String text) {
+        assert shouldHaveText(message,text,5);
+        return this;
+    }
+}
---
+++ src/test/java/com/ilcarro/stepDefinitions/RegistrationSteps.java
@@ -0,0 +1,53 @@
+package com.ilcarro.stepDefinitions;
+
+import com.ilcarro.pages.HomePage;
+import com.ilcarro.pages.RegistrationPage;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+
+
+import static com.ilcarro.pages.BasePage.driver;
+
+public class RegistrationSteps {
+
+    RegistrationPage registration;
+
+    @And("User clicks on Sign Up link")
+    public void click_on_sign_up_link() {
+        new HomePage(driver).clickOnSignUpLink();
+    }
+
+    @And("User enters data")
+    public void entersData() {
+        registration = new RegistrationPage(driver);
+        registration.enterRegistrationDetails("Olga", "Smith", "myliberty3@ukr.net", "Aa123456!");
+    }
+
+    @And("Check the box to agree to the terms of use and privacy policy")
+    public void check_the_box_to_agree() {
+        registration.checkTheBoxToAgree();
+    }
+
+    @And("User click on Yalla button")
+    public void clickOnYalla() {
+        registration.clickOnYallaButton();
+    }
+
+    @Then("User verifies that the success message is displayed")
+    public void success_Message() {
+        registration.successMessage("Registered");
+    }
+
+    @And("User enters data that already exists")
+    public void user_enters_data_that_already_exists () {
+        registration = new RegistrationPage(driver);
+        registration.userEntersExistingData();
+    }
+
+    @Then("User receives a message that the user already exists")
+    public void failure_message() {
+        registration.errorMessage("Registration failed");
+    }
+
+
+}
